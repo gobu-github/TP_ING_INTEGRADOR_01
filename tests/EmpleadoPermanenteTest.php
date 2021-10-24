4c30829f18243c7490364f6ff51d140f0b553c00
Gobu_Git <hgobulin.ds@gmail.com>
2021-10-24 01:58:53 -0300

    Se agregan los tests de empleadoPermanente

--- tests/EmpleadoPermanenteTest.php
+++ tests/EmpleadoPermanenteTest.php
@@ -21,5 +21,54 @@ class EmpleadoPermanenteTest extends EmpleadoTest
     }
 
     
-      
+    public function testCalculaLaComisionRecibida() 
+    
+    {
+    $fechaIngreso = new DateTime('1983-11-03');
+    $h = $this->crear("Hernán", "Gobu", 1000, 10000, $fechaIngreso);
+    $fechaActual = new DateTime();
+    $antiguedad = $fechaActual->diff($fechaIngreso);
+    $this->assertEquals($antiguedad->y, $h->calcularAntiguedad());
+    $this->assertEquals("{$antiguedad->y}%", $h->calcularComision());
+    } 
+    
+    public function testCalcularIngresoTotal() 
+    
+    {
+       $antiguedad = 50;
+       $h = $this->crear("Hernán", "Gobu", 1000, 10000 + $antiguedad);
+       $this->assertEquals(10050, $h->calcularIngresoTotal());
+    }
+
+
+    public function testCalculaLaAntiguedad() 
+    
+    {
+    $fechaIngreso = new DateTime('1983-11-03');
+    $h = $this->crear("Hernán,", "Gobulin", 123456, 1000, $fechaIngreso);
+    $fechaActual = new DateTime();
+    $antiguedad = $fechaActual->diff($fechaIngreso);
+    $this->assertEquals($antiguedad->y, $h->calcularAntiguedad());
+    }
+     
+    public function testSinFechaIngresoRetornaFechadelDiaYAntiguedad0()
+    
+    {
+    $fechaIngreso = new DateTime();
+    $h = $this->crear("Hernán", "Gobu", 1000, 10000, $fechaIngreso);
+    $fechaActual = new DateTime();
+    $antiguedad = $fechaActual->diff($fechaIngreso);
+    $this->assertEquals($antiguedad->y, $h->calcularAntiguedad());
+    $this->assertEquals("0%", $h->calcularComision());
+    }
+
+    public function testCalculaAntiguedadConFechaPosteriorALaDeHoy()
+     
+    {
+    $this->expectException(\Exception::class);
+    $fechaIngreso = new DateTime('2030-10-15');
+    $h = $this->crear("Hernán", "Gobu", 42214, 100, $fechaIngreso);
+    $fechaActual = new DateTime();
+    $antiguedad = $fechaActual->diff($fechaIngreso);
+    }
  }
